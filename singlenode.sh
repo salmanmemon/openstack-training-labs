5794bc59d555fe4cd44e80be5f78619128999bea
Pranav Salunke <dguitarbite@gmail.com>
2013-09-17 15:02:43 +0530

    Kickstarted Single Node
    Started with Single Node. Basic Flow of how these scripts are going
    to do the required is being given as comments in the singlenode.sh

--- singlenode.sh
+++ singlenode.sh
@@ -7,3 +7,47 @@
 # License: Apache Software License (ASL) 2.0 
 
 echo "Single Node"
+
+uname=
+pass=
+vm_id=
+# Download The VM From Repo/Storage Space on Internet.
+
+# Regester the VM & Start the VM.
+# Execute the Single Node Script to configure the network and getting Ubuntu
+# Ready for openstack.
+vboxmanage guestcontrol $vm_id execute --image "/bin/ls" --username $uname --password $pass --wait-exit --wait-stdout
+# Snapshot 1. Basic Settings
+
+# Execute the single node install scripts and take snapshots after each script
+# 1. Download all the packages using apt-get --download-only but not installing
+# them.
+# 2. Snapshot 2. Offline Ready
+
+
+# 3. Keystone
+# 4. Snapshot 3. Keystone Ready
+
+
+# 5. Glance
+# 6. Snapshot 4. Glance Ready
+
+
+# 7. Quantum
+# 8. Snapshot 5. Quantum Ready
+
+
+# 9. Nova
+# 10. Snapshot 6. Nova Ready
+
+
+# 11. Cinder
+# 12. Snapshot 7. Cinder Ready
+
+
+# 13. Horizon
+# 14. Snapshot 8. Horizon Ready
+
+
+# 15. Configure and Kickstart VM in OpenStack
+# 16. Snapshot 9. OpenStack Configured
