058fee7fafc8cfe38ee2fadc3f846c76872ab684
Pranav Salunke <dguitarbite@gmail.com>
2013-09-18 20:38:07 +0530

    Added a few lines to Pre-Install Script
    Trying to keep it a bit more modular by using
    functions.

--- Scripts/Keystone/Keystone.sh
+++ Scripts/Keystone/Keystone.sh
@@ -4,4 +4,48 @@
 #       meant for OpenStack Labs.
 #
 # Contact: pranav@aptira.com
-# License: Apache Software License (ASL) 2.0 
+# License: Apache Software License (ASL) 2.0
+
+###############################################################################
+#                                                                             #
+# This Script will install keystone related packages and after installaion, it#
+# will configure Keystone, populate the database.                             #
+#                                                                             #
+###############################################################################
+
+# Note: You DoNot Need Internet for this due to the magic of --download-only
+echo "Internet connection is not required for this script to run"
+
+pre_keystone(){
+    
+    # 1. Database - MySQL and Python MySQL DB Connector
+    apt-get install -y mysql-server python-mysqldb
+
+    # Configure MySQL to listen to other all IP addresses
+    sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/my.cnf
+
+    # Restart MySQL service
+    service mysql restart
+      
+    # 2. Install RabbitMQ
+    apt-get install -y rabbitmq-server
+    apt-get install -y ntp
+    apt-get install -y vlan bridge-utils
+
+    # Enable IP Forwarding
+    sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf
+    sysctl net.ipv4.ip_forward=1
+}
+
+keystone() {
+
+    # 1. Install Keystone
+    apt-get -y keystone
+
+    # 2. Create MySQL Database
+
+    # 3. Configure keystone scripts (copy the template file)
+
+    # 4. 
+
+}
--- Scripts/PreInstall/PreInstall.sh
+++ Scripts/PreInstall/PreInstall.sh
@@ -5,3 +5,74 @@
 #
 # Contact: pranav@aptira.com
 # License: Apache Software License (ASL) 2.0 
+
+###################################################
+# This Script will download the required packages #
+# using apt-get --download-only option.           #
+#                                                 #
+# This script will install and get your VM ready  #
+# for OpenStack.                                  #
+###################################################
+
+# Assumptions : It is assumed that internet is
+# switched on/VM has internet connection. 
+# Internet is required for this script to run.
+
+# Add a Few required packages
+apt-get install -y ubuntu-cloud-keyring python-software-properties software-properties-common python-keyring
+# Update apt-get
+apt-get -y update 
+
+# Add OpenStack Grizzly Repo.
+echo deb http://ubuntu-cloud.archive.canonical.com/ubuntu precise-updates/grizzly main >> /etc/apt/sources.list.d/grizzly.list
+
+# Update apt-get, Upgrade Installed Packages and Kernel Upgrade
+apt-get -y --download-only update
+apt-get -y --download-only upgrade
+apt-get -y --download-only dist-upgrade
+apt-get install -y --download-only ubuntu-cloud-keyring python-software-properties software-properties-common python-keyring
+
+# Configure the Network Interfaces by using the templates in the networking
+# part of this folder.
+
+# TO BE ADDED LATER ON.
+
+#service networking restart
+
+# Get (apt-get) all the packages and download them but not install them using
+# the --download-only option.
+# Advantage/Use/Need of --download-only in this particular case is due to the
+# weird use-case of teaching OpenStack by deploying OpenStack in a local/remote
+# location (College,Uni,Office,Conferences etc.) and the practise of keeping
+# all the packages downloaded but-not-installed eliminates the need for
+# internet while deploying openstack.
+
+single_node() {
+    #Install All package on the given Virtual Machine ...
+    apt-get install -y --download-only mysql-server python-mysqldb rabbitmq-server ntp vlan bridge-utils
+     keystone glance openvswitch-switch openvswitch-datapath-dkms quantum-server quantum-plugin-openvswitch quantum-plugin-openvswitch-agent dnsmasq quantum-dhcp-agent quantum-l3-agent
+      cpu-checker kvm libvirt-bin pm-utils nova-api nova-cert novnc nova-consoleauth nova-scheduler nova-novncproxy nova-doc nova-conductor nova-compute-kvm
+       cinder-api cinder-scheduler cinder-volume iscsitarget open-iscsi iscsitarget-dkms
+        openstack-dashboard memcached
+}
+
+multi_node(){
+    # Install packages as per the node defination ...
+
+    # TO BE DONE.
+
+    ## Also need to figure out whether to download all the packages even
+    ## if they are not playing any role in the given node to keep the scripts simpler
+}
+
+if [ "$1" == "single-node" ]; then
+    single_node
+else
+    if [ "$1" == "multi-node" ]; then
+        multi_node
+    else
+        echo "invalid option ... cannot proceede"
+    fi
+fi
+
+echo -e "Your VM is ready for OpenStack Installation ... \n you dont need internet from now on"
