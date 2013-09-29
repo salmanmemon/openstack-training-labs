57a4dc20ab82022d141154d95b1d24734c435fdd
pmoosh <pmoosh@yahoo.com>
2013-09-28 22:23:08 -0700

    corrected connection string

--- Scripts/Keystone/Scripts/Credentials.sh
+++ Scripts/Keystone/Scripts/Credentials.sh
@@ -1,4 +1,4 @@
 export OS_TENANT_NAME=admin
 export OS_USERNAME=admin
 export OS_PASSWORD=admin_pass
-export OS_AUTH_URL="http://192.168.100.51:5000/v2.0/"
+export OS_AUTH_URL="http://10.10.10.51:5000/v2.0/"
